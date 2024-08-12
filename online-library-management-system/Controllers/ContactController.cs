4f2c4e3af574ad5975cc19166887d36f24eb1693
madu12 <sameeramadushan0812@gmail.com>
2024-08-13 00:43:49 +0530

    Contact Us page

---
+++ online-library-management-system/Controllers/ContactController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace online_library_management_system.Controllers
+{
+    public class ContactController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
